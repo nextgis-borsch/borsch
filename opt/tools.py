8bec86e31a2d990005a2f5a1a4fedf1068566822
Dmitry Baryshnikov <bishop.dev@gmail.com>
2017-03-13 17:57:17 +0300

    Add cgal and boost to Mac OS X build

--- opt/tools.py
+++ opt/tools.py
@@ -23,8 +23,8 @@ import csv
 repositories = [
     {"url" : "borsch", "cmake_dir" : "cmake", "build" : [], "args" : []},
     {"url" : "googletest", "cmake_dir" : "cmake", "build" : [], "args" : []},
-    {"url" : "lib_boost", "cmake_dir" : "cmake", "build" : [], "args" : []},
-    {"url" : "lib_cgal", "cmake_dir" : "cmake", "build" : [], "args" : []},
+    {"url" : "lib_boost", "cmake_dir" : "cmake", "build" : ["mac"], "args" : []},
+    {"url" : "lib_cgal", "cmake_dir" : "cmake", "build" : ["mac"], "args" : ['-DBUILD_TESTING=OFF', '-DWITH_CPACK=OFF']},
     {"url" : "lib_xml2", "cmake_dir" : "cmake", "build" : ["win"], "args" : []},
     {"url" : "lib_z", "cmake_dir" : "cmake", "build" : ["win"], "args" : []},
     {"url" : "lib_openssl", "cmake_dir" : "cmake", "build" : ["mac", "win"], "args" : ['-DOPENSSL_NO_DYNAMIC_ENGINE=ON']},
