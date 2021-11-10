5cf65a2ab907e9a1be29954776fe3f86d1184a75
Dmitry Baryshnikov <bishop.dev@gmail.com>
2021-11-10 21:31:00 +0300

    Fix post in repka

--- opt/repka_release.py
+++ opt/repka_release.py
@@ -152,7 +152,7 @@ def create_release(packet_id, name, description, tag, file_uid, file_name, usern
         "files": [
             {"upload_name": file_uid, "name": file_name},
         ]
-    })
+    }).decode()
     clen = len(data)
 
     request = urllib2.Request(url, data=data, headers={'Content-Type': 'application/json', 'Content-Length': clen})
