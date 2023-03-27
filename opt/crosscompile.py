b0e14288e4bcdf6882dde2f8876e824d7d4b6a56
d3n15 <x514ckw4r3@gmail.com>
2023-03-28 00:28:59 +0300

    Fixed build dir in crosscompile.py

--- opt/crosscompile.py
+++ opt/crosscompile.py
@@ -104,7 +104,9 @@ if __name__ == "__main__":
     parser.add_argument('--packages', dest='packages', help='packages list separated by semicolon')
 
     args = parser.parse_args()
-        
+    
+    root_dir = os.getcwd()
+    
     for repo in get_packages(args.packages):
         for abi in abis:
-            make_package(repo, os.getcwd(), abi, args.login, args.password)
+            make_package(repo, root_dir, abi, args.login, args.password)
